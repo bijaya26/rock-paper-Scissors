72b7131414833bb56b2bf48bb8794d9cdbe02cff
Bijaya <bijaypokharel78@gmail.com>
2026-09-03 17:30:59 +0100

    UI features added

--- javascript.js
+++ javascript.js
@@ -1,106 +1,115 @@
 
-//function computer choice created using math.random//
-function getComputerChoice(){
-    let choice=Math.random();
-    if(choice<=0.33){
-        return "rock";
-    }
-    else if(choice<=0.66){
-        return"paper";
-    }
+const div =document.querySelector("div");
+const body=document.querySelector("body");
+const container=document.createElement("div");
+body.appendChild(container);
+const paragraph=document.createElement("p");
+const score=document.createElement("p")
 
-    else{
-        return"scissors"
-    }
+container.appendChild(paragraph);
+container.appendChild(score);
 
-}
-//function humanChoice created that take human choice//
-
-function getHumanChoice(){
-    return prompt("Which one you choose").toLowerCase();
-}
-
-//global variable created//
-
+let humanChoice="";
 let humanScore=0;
 let computerScore=0;
 
-//function playround created//
+div.addEventListener("click",(e)=>{
+    let choice=e.target.dataset.choice;
+    humanChoice=choice;
+    
+    let computerSelection=getComputerChoice();
+    let human=humanselect(humanChoice);
+    playRound(human,computerSelection);
+   
+    
+   
+ 
 
-function playRound(humanChoice,computerChoice){
-    if(computerChoice==="rock" && humanChoice==="rock"){
-    console.log("draw");
-    }
-    else if (computerChoice==="scissors" && humanChoice==="scissors"){
-        console.log("draw")
-    }
-    else if(computerChoice==="paper" && humanChoice==="paper"){
-        console.log("draw");
-    }
 
-    else if(computerChoice==="rock" && humanChoice==="paper"){
-        humanScore++;
-        console.log("user win");
+    
+    
 
-    }
 
-    else if(computerChoice==="rock" && humanChoice==="scissors"){
-        computerScore++;
-        console.log("computer win");
+})
 
-    }
 
-    else if(computerChoice==="scissors" && humanChoice==="paper"){
-        computerScore++;
-        console.log("computer win");
-    }
-
-    else if (computerChoice=="paper" && humanChoice==="rock"){
-        computerScore++;
-        console.log("computer win");
-    }
-
-    else if (computerChoice==="paper"&&humanChoice==="scissors"){
-        humanScore++;
-        console.log("user win");
-    }
-
-    else if(computerChoice==="scissors" && humanChoice==="rock"){
-        humanScore++;
-        console.log("userWin");
-    }
-
-    console.log(`Total computer number is:${computerScore}`);
-    console.log(`Total user number is:${humanScore}`);
-
+function humanselect(humanSelection){
+    return humanSelection;
 
 }
 
-//playgame function created//
 
 
-function playGame(){
-    for(let i=1;i<=5;i++){
-    const humanSelection=getHumanChoice();
-    const computerSelection=getComputerChoice();
-    playRound(humanSelection,computerSelection);
-}
-
-    if(computerScore>humanScore){
-        console.log("sorry computer win");
+function getComputerChoice(){
+    let randomInt=Math.random();
+    if(randomInt<=0.33){
+        return "rock";
     }
-
-    else if(computerScore<humanScore){
-        console.log("Congratulations you win")
+    else if(randomInt<=0.66){
+        return "paper"
     }
     else{
-        console.log("sorry match is draw");
+        return "scissors";
+    }
+}
+
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice==="rock" && computerChoice==="rock"){
+        paragraph.textContent="Match Draw";
     }
 
-}
-//playgame function called//
+    else if(humanChoice==="rock" && computerChoice==="paper"){
+        humanScore++;
+        
+        paragraph.textContent="human Win";
+        
+    }
 
-playGame();
+    else if(humanChoice==="rock" && computerChoice==="scissors"){
+        computerScore++;
+        paragraph.textContent="computerWin";
+        
+    }
+
+    else if(humanChoice==="paper" && computerChoice==="rock"){
+        humanScore++;
+        paragraph.textContent="humanWin";
+        
+    }
+    else if(humanChoice==="paper" && computerChoice==="paper"){
+        paragraph.textContent="Match draw";
+    }
+    else if(humanChoice==="paper" && computerChoice==="scissors"){
+        computerScore++;
+        paragraph.textContent="computerWin";
+        
+    }
+    else if(humanChoice==="scissors" && computerChoice==="rock"){
+        computerScore++;
+        paragraph.textContent="computer win";
+        
+    }
+    else if(humanChoice==="scissors" && computerChoice==="paper"){
+        humanScore++;
+        paragraph.textContent="humanWin";
+        
+    }
+
+    else if(humanChoice==="scissors" && computerChoice==="scissors"){
+        paragraph.textContent="Match draw";
+    }
+    score.textContent=`Player: ${humanScore} | Computer: ${computerScore}`
+
+}
+
+
+
+
+
+
+
+
 
 
 
